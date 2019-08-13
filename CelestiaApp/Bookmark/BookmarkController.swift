791b5cd2bacaa96340c8e4cab4a5fa4bb8d49975
Li Linfeng <lilinfeng@tp-link.com.cn>
2019-08-13 17:48:03 +0800

    add bookmark reading

--- CelestiaApp/Bookmark/BookmarkController.swift
+++ CelestiaApp/Bookmark/BookmarkController.swift
@@ -9,7 +9,21 @@
 import Cocoa
 
 class BookmarkController: NSObject {
+    private var storedBookmarks: [BookmarkNode] = []
+
     func readBookmarks() {
+        guard let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first else {
+            return
+        }
+        let bookmarkFilePath = "\(path)/bookmark.json"
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: bookmarkFilePath))
+            let bookmarks = try JSONDecoder().decode([BookmarkNode].self, from: data)
+            storedBookmarks = bookmarks
+            // TODO: build up the bookmark menu
+        } catch let error {
+            print("Bookmark reading error: \(error.localizedDescription)")
+        }
     }
 
     @IBAction private func addBookmark(_ sender: Any) {
