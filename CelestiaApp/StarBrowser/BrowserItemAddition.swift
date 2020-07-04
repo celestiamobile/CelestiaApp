c8bd32d5b739637f662a06b56399ad8329c1eede
Levin Li <lilinfeng303@outlook.com>
2020-07-04 11:14:16 +0800

    Update copyright

--- CelestiaApp/StarBrowser/BrowserItemAddition.swift
+++ CelestiaApp/StarBrowser/BrowserItemAddition.swift
@@ -1,9 +1,12 @@
 //
-//  BrowserItemAddition.swift
-//  Celestia
+// BrowserItemAddition.swift
 //
-//  Created by Li Linfeng on 2019/11/21.
-//  Copyright © 2019 李林峰. All rights reserved.
+// Copyright © 2020 Celestia Development Team. All rights reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
 //
 
 import CelestiaCore
--- CelestiaApp/Utils/LegacyMigrator.h
+++ CelestiaApp/Utils/LegacyMigrator.h
@@ -1,8 +1,12 @@
 //
-//  Migrator.h
-//  Celestia
+// LegacyMigrator.h
 //
-//  Created by 李林峰 on 2019/10/7.
+// Copyright © 2020 Celestia Development Team. All rights reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
 //
 
 #import <Foundation/Foundation.h>
