50d6b386935ac5a17ca31e78e5e26e9bd1e9ee2d
Levin Li <lilinfeng303@outlook.com>
2021-06-02 21:09:24 +0800

    test appcenter

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,20 +2,16 @@
 
 cd $APPCENTER_SOURCE_DIRECTORY/..
 
-echo "Building for branch $APPCENTER_BRANCH"
-
 # Clone the Celestia repo (modified)
-git clone https://github.com/${GITHUB_USERNAME}/Celestia --branch $APPCENTER_BRANCH --single-branch
+git clone https://github.com/levinli303/Celestia --branch releases/1.3 --depth=1
 cd Celestia
 git submodule update --init
 cd ..
 
 # Clone the CelestiaCore repo
-git clone https://${GITHUB_USERNAME}:${GITHUB_ACCESS_TOKEN}@github.com/${GITHUB_USERNAME}/CelestiaCore --branch $APPCENTER_BRANCH --single-branch
+git clone https://github.com/levinli303/CelestiaCore --branch releases/1.3 --depth=1
 cd CelestiaCore
 git submodule update --init
-ln -sf libs/dependency/mac thirdparty
-cd ..
 
 # Install gettext, needed for translation
 brew install gettext
